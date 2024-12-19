86e0bf20a3a4d1f773688df9486f263a5f105d8e
Raikyuujm <115963599+Raikyuujm@users.noreply.github.com>
2024-12-19 14:55:09 -0300

    Add files via upload

---
+++ Teste1/index.js
@@ -0,0 +1,10 @@
+let nome = "";
+
+/*console.log(nome);
+console.error(nome);
+console.warn(nome);
+*/
+nome = window.prompt("Insira seu nome");
+window.alert("Você é um buceteiro de mão cheia " + nome);
+
+// let  confirmacao = window.confirm("Você está certo disso")
